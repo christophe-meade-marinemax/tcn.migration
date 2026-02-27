f6dfc0ca1f0cb0c7c3a30ca36163b7a2db36258e
christophe-meade-marinemax <christophe.meade@marinemax.com>
2026-02-27 11:52:04 +0100

    Update migration files

--- extracted/candidates/convert-to-xls.py
+++ extracted/candidates/convert-to-xls.py
@@ -1,10 +1,10 @@
 import json
 import pandas as pd
 
-with open("candidates-attachments-10.json", encoding="utf-8") as f:
+with open("candidates-9.json", encoding="utf-8") as f:
     data = json.load(f)
 
 df = pd.DataFrame(data)
 
 #transform the data to .xlsx format
-df.to_excel("candidates-attachments-10.xlsx", index=False)
+df.to_excel("candidates-9.xlsx", index=False)
